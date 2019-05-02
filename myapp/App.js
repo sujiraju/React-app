aa8c7715d2d2fb613fecf021bd55fa1e0bd4dfec
Sujiraju <sujiraju16@gmail.com>
2019-05-02 15:05:46 +0530

    adding events

--- myapp/App.js
+++ myapp/App.js
@@ -5,9 +5,9 @@
  * @format
  * @flow
  */
-// my new app commmit 2
+
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform,TextInput, StyleSheet, Text, View, Button,Alert} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -18,12 +18,49 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+  constructor(props) {
+    super(props);
+    this.state = { text: '',text1: '',text2: '',text3: '',text4:'' };
+  }
+ 
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+ <TextInput
+        style={{height: 40,width:80, borderColor: 'red', borderWidth: 1,justifyContent: 'space-between'}}
+        onChangeText={(text) => this.setState({text})}
+        value={this.state.text}
+  />
+    <TextInput
+      style={{height: 40,width:80, borderColor: 'red', borderWidth: 1}}
+      onChangeText={(text1) => this.setState({text1})}
+      value={this.state.text1}
+    />
+
+    <TextInput
+    style={{height: 40,width:80, borderColor: 'red', borderWidth: 1}}
+    onChangeText={(text2) => this.setState({text2})}
+    value={this.state.text2}
+  />
+  <TextInput
+  style={{height: 40,width:80, borderColor: 'red', borderWidth: 1}}
+  onChangeText={(text3) => this.setState({text3})}
+  value={this.state.text3}
+/>
+<TextInput
+style={{height: 40,width:80, borderColor: 'red', borderWidth: 1}}
+onChangeText={(text4) => this.setState({text4})}
+value={this.state.text4}
+/>
+
+      <Button
+      onPress={() => {
+        Alert.alert('You tapped the button!'+this.state.text);
+      }}
+      title="Learn More"
+      color="#841584"
+        />
       </View>
     );
   }
@@ -32,7 +69,7 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'space-between',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
