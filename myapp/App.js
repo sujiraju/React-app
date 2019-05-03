42241c7067953ca9d973216094f1c2204799357d
Sujiraju <sujiraju16@gmail.com>
2019-05-03 08:02:11 +0530

    sqlite code1

--- myapp/App.js
+++ myapp/App.js
@@ -7,61 +7,132 @@
  */
 
 import React, {Component} from 'react';
-import {Platform,TextInput, StyleSheet, Text, View, Button,Alert} from 'react-native';
+import { openDatabase } from 'react-native-sqlite-storage';
+import {Platform,TextInput,TouchableOpacity, StyleSheet, Text, View, Button,Alert,ScrollView,KeyboardAvoidingView} from 'react-native';
+var db = openDatabase({ name: 'events.db' });
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
+const Mybutton = props => {
+  return (
+    <TouchableOpacity style={styles.button} onPress={props.customClick}>
+      <Text style={styles.text}>{props.title}</Text>
+    </TouchableOpacity>
+  );
+};
 type Props = {};
 export default class App extends Component<Props> {
   constructor(props) {
     super(props);
-    this.state = { text: '',text1: '',text2: '',text3: '',text4:'' };
+    this.state = { 
+      Breakfast: '',
+      Initial_Amount: '',
+      Lunch: '',
+      Snacks: '',
+      Dinner:'',
+      Balance:'',
+      petrol:'' };
   }
- 
+  register_user = () => {
+    
+    var that = this;
+    const { Breakfast } = this.state;
+    const { Initial_Amount } = this.state;
+    const { Lunch } = this.state;
+    const {Snacks} = this.state;
+    const {Dinner} = this.state;
+    const {Balance} = this.state;
+    const {petrol} = this.state;
+
+Alert(Breakfast, Initial_Amount, Lunch,Snacks,Dinner);
+    if (Breakfast) {
+      if (Initial_Amount) {
+        if (Lunch) {
+          db.transaction(function(tx) {
+            tx.executeSql(
+              'INSERT INTO  (Breakfast, Initial_Amount, Lunch,Snacks,Dinner,Balance,petrol) VALUES (?,?,?)',
+              [Breakfast,  Initial_Amount, Lunch,Snacks,Dinner,Balance,petrol],
+              (tx, results) => {
+                console.log('Results', results.rowsAffected);
+                if (results.rowsAffected > 0) {
+                  Alert.alert(
+                    'Success',
+                    'You are Registered Successfully',
+                    [
+                      {
+                        text: 'Ok',
+                        onPress: () =>
+                          that.props.navigation.navigate('HomeScreen'),
+                      },
+                    ],
+                    { cancelable: false }
+                  );
+                } else {
+                  alert('Registration Failed');
+                }
+              }
+            );
+          });
+        } else {
+          alert('Please fill Address');
+        }
+      } else {
+        alert('Please fill Contact Number');
+      }
+    } else {
+      alert('Please fill Name');
+    }
+  };
+
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
- <TextInput
-        style={{height: 40,width:80, borderColor: 'red', borderWidth: 1,justifyContent: 'space-between'}}
-        onChangeText={(text) => this.setState({text})}
-        value={this.state.text}
+     <ScrollView keyboardShouldPersistTaps="handled"> 
+     <KeyboardAvoidingView
+            behavior="padding"
+            style={{ flex: 1, justifyContent: 'space-between' }}>
+       <Text style={styles.welcome}>Welcome to React Native!</Text>
+ 
+       <TextInput
+        style={{height: 40,width:80, borderColor: 'red', borderWidth: 1}}
+        onChangeText={( Breakfast) => this.setState({ Breakfast})}
+     
   />
-    <TextInput
-      style={{height: 40,width:80, borderColor: 'red', borderWidth: 1}}
-      onChangeText={(text1) => this.setState({text1})}
-      value={this.state.text1}
-    />
 
     <TextInput
+      style={{height: 40,width:80, borderColor: 'red', borderWidth: 1}}
+      onChangeText={(Initial_Amount) => this.setState({Initial_Amount})}
+      
+/>
+    <TextInput
     style={{height: 40,width:80, borderColor: 'red', borderWidth: 1}}
-    onChangeText={(text2) => this.setState({text2})}
-    value={this.state.text2}
+    onChangeText={(Lunch) => this.setState({Lunch})}
   />
   <TextInput
   style={{height: 40,width:80, borderColor: 'red', borderWidth: 1}}
-  onChangeText={(text3) => this.setState({text3})}
-  value={this.state.text3}
+  onChangeText={(Snacks) => this.setState({Snacks})}
+  
 />
 <TextInput
-style={{height: 40,width:80, borderColor: 'red', borderWidth: 1}}
-onChangeText={(text4) => this.setState({text4})}
-value={this.state.text4}
+style={{height: 40,width:80, borderColor: 'red', borderWidth: 1, }}
+onChangeText={(Dinner) => this.setState({Dinner})}
 />
+<TextInput
+        style={{height: 40,width:80, borderColor: 'red', borderWidth: 1}}
+        onChangeText={(Balance) => this.setState({Balance})}
+       
+  />
+    <TextInput
+      style={{height: 40,width:80, borderColor: 'red', borderWidth: 1}}
+      onChangeText={(petrol) => this.setState({petrol})}
+          />
 
-      <Button
-      onPress={() => {
-        Alert.alert('You tapped the button!'+this.state.text);
-      }}
-      title="Learn More"
-      color="#841584"
-        />
-      </View>
+        <Mybutton
+        title="Store events"
+        customClick={this.register_user.bind(this)}
+          />
+      
+        </KeyboardAvoidingView>
+        </ScrollView>
+     
     );
   }
 }
@@ -69,7 +140,7 @@ value={this.state.text4}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'space-between',
+    justifyContent:'space-evenly',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
