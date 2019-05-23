53ff94b45ca62614ad1c5251b89cfebbe7e9aaa7
Sujiraju <sujiraju16@gmail.com>
2019-05-23 07:31:34 +0530

    store data to database, get and view stored data and implement stack navigation

--- app/android/app/src/main/java/com/app/MainApplication.java
+++ app/android/app/src/main/java/com/app/MainApplication.java
@@ -3,6 +3,7 @@ package com.app;
 import android.app.Application;
 import org.pgsqlite.SQLitePluginPackage;
 import com.facebook.react.ReactApplication;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -23,7 +24,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
         new SQLitePluginPackage(),
-          new MainReactPackage()
+          new MainReactPackage(),
+            new RNGestureHandlerPackage()
       );
     }
 
